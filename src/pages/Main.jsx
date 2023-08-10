74a99d3c4da4dbc70c6875021ee351f845ea5f99
devjjin <38846447+devjjin@users.noreply.github.com>
2023-08-10 21:08:45 +0900

    feat: add redirect path

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,24 +3,35 @@ import { useNavigate } from 'react-router-dom';
 
 export default function Main() {
   const navigate = useNavigate();
+  const token = localStorage.getItem('accessToken');
 
-  const goToSignUp = () => {
-    navigate('/signup');
+  const goPath = (path) => {
+    const goToTodo = () => {
+      navigate('/todo');
+    };
+    if (token) {
+      goToTodo();
+    } else {
+      navigate(path);
+    }
   };
 
-  const goToSignIn = () => {
-    navigate('/signin');
+  const goTodo = () => {
+    goPath('/signin');
   };
 
   return (
     <div>
       <h2>My TodoList</h2>
       <div>
-        <button type="button" onClick={goToSignUp}>
+        <button type="button" onClick={() => goPath('/signup')}>
           회원가입
         </button>
-        <button type="button" onClick={goToSignIn}>
+        <button type="button" onClick={() => goPath('/signin')}>
           로그인
+        </button>
+        <button type="button" onClick={goTodo}>
+          todo로
         </button>
       </div>
     </div>
