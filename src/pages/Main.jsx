66ed991b09950327bc5eb57d1ed4b7c9c465fa5f
devjjin <38846447+devjjin@users.noreply.github.com>
2023-08-11 03:26:38 +0900

    design: main page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 export default function Main() {
   const navigate = useNavigate();
@@ -21,19 +22,51 @@ export default function Main() {
   };
 
   return (
-    <div>
+    <Container>
       <h2>My TodoList</h2>
-      <div>
-        <button type="button" onClick={() => goPath('/signup')}>
+      <ButtonWrap>
+        <SignUpBtn type="button" onClick={() => goPath('/signup')}>
           회원가입
-        </button>
-        <button type="button" onClick={() => goPath('/signin')}>
+        </SignUpBtn>
+        <SignInBtn type="button" onClick={() => goPath('/signin')}>
           로그인
-        </button>
-        <button type="button" onClick={goTodo}>
+        </SignInBtn>
+        <Button type="button" onClick={goTodo}>
           todo로
-        </button>
-      </div>
-    </div>
+        </Button>
+      </ButtonWrap>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const ButtonWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+`;
+
+const Button = styled.button`
+  margin-top: 10px;
+  width: 100px;
+  height: 30px;
+  border: none;
+  background-color: #3498db;
+  color: #fff;
+  cursor: pointer;
+`;
+
+const SignUpBtn = styled(Button)`
+  background-color: #e74c3c;
+`;
+
+const SignInBtn = styled(Button)`
+  background-color: #2ecc71;
+`;
