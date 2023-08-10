060d10275d62492bacb30636d7a08589db552dfa
devjjin <38846447+devjjin@users.noreply.github.com>
2023-08-10 20:51:14 +0900

    feat: Add to redirect sign page in main

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Main() {
-  return <div>Main</div>;
+  const navigate = useNavigate();
+
+  const goToSignUp = () => {
+    navigate('/signup');
+  };
+
+  const goToSignIn = () => {
+    navigate('/signin');
+  };
+
+  return (
+    <div>
+      <h2>My TodoList</h2>
+      <div>
+        <button type="button" onClick={goToSignUp}>
+          회원가입
+        </button>
+        <button type="button" onClick={goToSignIn}>
+          로그인
+        </button>
+      </div>
+    </div>
+  );
 }
